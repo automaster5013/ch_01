1f7718ecdf5be73cadde3e5ed3e08a4d9de2f5d5
myeonghwi <automaster5013@gmail.com>
2026-04-02 12:34:53 +0900

    python_보기_좋은_차트_만들기_소스코드

--- step_3_2.py
+++ step_3_2.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import matplotlib.pyplot as plt
-from step_2_1 import OUT_DIR
+from step_2_1 import OUT_DIR    # 이전에 작성한 모듈을 불러옵니다.
 from step_3_1 import load_plot_data
 
 plot_data = load_plot_data()
